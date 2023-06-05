b26b6f8ac464fb46d07c0f30573da37944ea2503
Juan Jose Chiroque <juanjosechiroque@gmail.com>
2023-06-04 22:04:19 -0500

    feat: add tailwind css

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -15,17 +15,23 @@ function TaskForm() {
     }
 
     return (
-        <form>
-            <input
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                placeholder='Escribe tu tarea' />
-            <textarea
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-            ></textarea>
-            <button onClick={handleClick}>Guardar</button>
-        </form>
+        <div className="max-w-md mx-auto">
+            <form className='bg-slate-800 p-10 mb-4'>
+                <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>
+                <input
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    placeholder='Escribe tu tarea'
+                    className='bg-slate-300 p-3 w-full mb-2'
+                />
+                <textarea
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                    className='bg-slate-300 p-3 w-full mb-2'
+                ></textarea>
+                <button className='bg-indigo-500 px-3 py-1 text-white' onClick={handleClick}>Guardar</button>
+            </form>
+        </div>
     )
 }
 
